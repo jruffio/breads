848a06130085ff9698d0fbe2adc002a55a8e2948
Shubh Agrawal <shubhagrawal30@gmail.com>
2021-07-15 23:35:04 -0400

    changed badpix code

--- breads/utils.py
+++ breads/utils.py
@@ -12,113 +12,120 @@ from py.path import local
 from scipy.stats import median_absolute_deviation
 from scipy.optimize import lsq_linear
 from scipy.signal import correlate2d
+from breads.instruments.instrument import Instrument
 
 
 def _task_findbadpix(paras):
-    data_arr,noise_arr,badpix_arr,med_spec,M_spline = paras
+    data_arr, noise_arr, badpix_arr, med_spec, M_spline = paras
     new_data_arr = np.array(copy(data_arr), '<f4')#.byteswap().newbyteorder()
     new_badpix_arr = copy(badpix_arr)
     res = np.zeros(data_arr.shape) + np.nan
     for k in range(data_arr.shape[1]):
-        where_data_finite = np.where(np.isfinite(badpix_arr[:,k])*np.isfinite(data_arr[:,k])*np.isfinite(noise_arr[:,k])*(noise_arr[:,k]!=0))
+        where_data_finite = \
+            np.where(np.isfinite(badpix_arr[:, k]) * np.isfinite(data_arr[:,k]) * \
+                np.isfinite(noise_arr[:, k]) * (noise_arr[:,k]!=0))
         if np.size(where_data_finite[0]) == 0:
-            res[:,k] = np.nan
+            res[:, k] = np.nan
             continue
-        d = data_arr[where_data_finite[0],k]
-        d_err = noise_arr[where_data_finite[0],k]
+        d = data_arr[where_data_finite[0], k]
+        d_err = noise_arr[where_data_finite[0], k]
 
-        M = M_spline[where_data_finite[0],:]*med_spec[where_data_finite[0],None]
+        M = M_spline[where_data_finite[0],:] * med_spec[where_data_finite[0], None]
 
 
-        bounds_min = [0, ]* M.shape[1]
+        bounds_min = [0, ] * M.shape[1]
         bounds_max = [np.inf, ] * M.shape[1]
-        p = lsq_linear(M/d_err[:,None],d/d_err,bounds=(bounds_min, bounds_max)).x
+        p = lsq_linear(M / d_err[:,None], d / d_err, bounds=(bounds_min, bounds_max)).x
         # p,chi2,rank,s = np.linalg.lstsq(M/d_err[:,None],d/d_err,rcond=None)
-        m = np.dot(M,p)
-        res[where_data_finite[0],k] = d-m
+        m = np.dot(M, p)
+        res[where_data_finite[0], k] = d - m
 
         # where_bad = np.where((np.abs(res[:,k])>3*np.nanstd(res[:,k])) | np.isnan(res[:,k]))
-        where_bad = np.where((np.abs(res[:,k])>3*median_absolute_deviation(res[where_data_finite[0],k])) | np.isnan(res[:,k]))
-        new_badpix_arr[where_bad[0],k] = np.nan
-        where_bad = np.where(np.isnan(np.correlate(new_badpix_arr[:,k] ,np.ones(2),mode="same")))
-        new_badpix_arr[where_bad[0],k] = np.nan
-        new_data_arr[where_bad[0],k] = np.nan
+        where_bad = np.where((np.abs(res[:, k]) > 3 * \
+            median_absolute_deviation(res[where_data_finite[0], k])) | np.isnan(res[:, k]))
+        new_badpix_arr[where_bad[0], k] = np.nan
+        where_bad = np.where(np.isnan(np.correlate(new_badpix_arr[:, k] ,np.ones(2),mode="same")))
+        new_badpix_arr[where_bad[0], k] = np.nan
+        new_data_arr[where_bad[0], k] = np.nan
 
-        new_data_arr[:,k] = np.array(pd.DataFrame(new_data_arr[:,k]).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))[:, 0]
+        new_data_arr[:, k] = np.array(pd.DataFrame(new_data_arr[:, \
+                k]).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))[:, 0]
 
-    return new_data_arr,new_badpix_arr,res
+    return new_data_arr, new_badpix_arr, res
 
 def _remove_edges(paras):
-    slices,nan_mask_boxsize = paras
+    slices, nan_mask_boxsize = paras
     cp_slices = copy(slices)
     for slice in cp_slices:
         slice[np.where(slice==0)] = np.nan
-        slice[np.where(np.isnan(correlate2d(slice,np.ones((nan_mask_boxsize,nan_mask_boxsize)),mode="same")))] = np.nan
-    cp_slices[:,0:nan_mask_boxsize//2,:] = np.nan
-    cp_slices[:,-nan_mask_boxsize//2+1::,:] = np.nan
-    cp_slices[:,:,0:nan_mask_boxsize//2] = np.nan
-    cp_slices[:,:,-nan_mask_boxsize//2+1::] = np.nan
+        slice[np.where(np.isnan(correlate2d(slice, \
+            np.ones((nan_mask_boxsize,nan_mask_boxsize)), mode="same")))] = np.nan
+    cp_slices[:, 0:nan_mask_boxsize//2,:] = np.nan
+    cp_slices[:, -nan_mask_boxsize//2+1::,:] = np.nan
+    cp_slices[:, :, 0:nan_mask_boxsize//2] = np.nan
+    cp_slices[:, :, -nan_mask_boxsize//2+1::] = np.nan
 
     return cp_slices
 
 
-def findbadpix(cube, noisecube=None, badpixcube=None,chunks=20,mypool=None,med_spec=None,nan_mask_boxsize=3):
-    if noisecube is None:
-        noisecube = np.ones(cube.shape)
-    if badpixcube is None:
-        badpixcube = np.ones(cube.shape)
+def findbadpix(data: Instrument, chunks=20, mypool=None, med_spec=None, nan_mask_boxsize=3):
+    if data.noise is None:
+        noise = np.ones(data.data.shape)
+    else:
+        noise = data.noise
+    if data.bad_pixels is None:
+        bad_pixels = np.ones(data.data.shape)
+    else:
+        bad_pixels = data.bad_pixels
 
-    new_cube = copy(cube)
-    new_badpixcube = copy(badpixcube)
-    nz,ny,nx = cube.shape
-    res = np.zeros(cube.shape) + np.nan
+    nz, ny, nx = data.data.shape
+    res = np.zeros(data.data.shape) + np.nan
 
     x = np.arange(nz)
-    x_knots = x[np.linspace(0,nz-1,chunks+1,endpoint=True).astype(np.int)]
-    M_spline = get_spline_model(x_knots,x,spline_degree=3)
+    x_knots = x[np.linspace(0, nz-1, chunks+1, endpoint=True).astype(np.int)]
+    M_spline = get_spline_model(x_knots, x, spline_degree=3)
 
-    N_valid_pix = ny*nx
+    N_valid_pix = ny * nx
     if med_spec is None:
-        _cube = copy(cube)
-        _cube[np.where(_cube<=0)] = np.nan
-        med_spec = np.nanmedian(cube,axis=(1,2))
-    new_badpixcube[np.where(cube==0)] = np.nan
+        med_spec = np.nanmedian(data.data, axis=(1, 2))
+    data.bad_pixels[np.where(data.data == 0)] = np.nan
 
     # plt.plot(med_spec)
     # plt.show()
 
     if mypool is None:
-        new_badpixcube = _remove_edges((new_badpixcube,nan_mask_boxsize))
+        data.bad_pixels = _remove_edges((data.bad_pixels, nan_mask_boxsize))
     else:
         numthreads = mypool._processes
-        chunk_size = nz//(3*numthreads)
-        N_chunks = nz//chunk_size
+        chunk_size = nz // (3*numthreads)
+        N_chunks = nz // chunk_size
         wvs_indices_list = []
         slices_list = []
         for k in range(N_chunks-1):
-            slices_list.append(new_badpixcube[(k*chunk_size):((k+1)*chunk_size)])
-            wvs_indices_list.append(np.arange((k*chunk_size),((k+1)*chunk_size)))
-        slices_list.append(new_badpixcube[((N_chunks-1)*chunk_size):nz])
-        wvs_indices_list.append(np.arange(((N_chunks-1)*chunk_size),nz))
+            slices_list.append(data.bad_pixels[(k*chunk_size):((k+1)*chunk_size)])
+            wvs_indices_list.append(np.arange((k*chunk_size), ((k+1)*chunk_size)))
+        slices_list.append(data.bad_pixels[((N_chunks-1)*chunk_size):nz])
+        wvs_indices_list.append(np.arange(((N_chunks-1)*chunk_size), nz))
 
         outputs_list = mypool.map(_remove_edges, zip(slices_list,itertools.repeat(nan_mask_boxsize)))
-        #save it to shared memory
+        # save it to shared memory
         for indices, out in zip(wvs_indices_list,outputs_list):
-            new_badpixcube[indices,:,:] = out
+            data.bad_pixels[indices, :, :] = out
 
 
     if mypool is None:
-        data_list = np.reshape(new_cube,(nz,nx*ny))
-        noise_list = np.reshape(noisecube,(nz,nx*ny))
-        badpix_list = np.reshape(new_badpixcube,(nz,nx*ny))
-        out_data,out_badpix,out_res = _task_findbadpix((data_list,noise_list,badpix_list,med_spec,M_spline))
-        new_cube = np.reshape(new_cube,(nz,ny,nx))
-        new_badpixcube = np.reshape(new_badpixcube,(nz,ny,nx))
-        res = np.reshape(out_res,(nz,ny,nx))
+        data_list = np.reshape(data.data, (nz, nx*ny))
+        noise_list = np.reshape(noise, (nz, nx*ny))
+        badpix_list = np.reshape(bad_pixels, (nz, nx*ny))
+        out_data, out_badpix, out_res = \
+            _task_findbadpix((data_list, noise_list, badpix_list, med_spec, M_spline))
+        data.data = np.reshape(out_data, (nz, ny, nx))
+        data.bad_pixels = np.reshape(out_badpix, (nz, ny, nx))
+        res = np.reshape(out_res, (nz, ny, nx))
     else:
         numthreads = mypool._processes
-        chunk_size = N_valid_pix//(3*numthreads)
-        wherenotnans = np.where(np.nansum(np.isfinite(badpixcube),axis=0)!=0)
+        chunk_size = N_valid_pix // (3*numthreads)
+        wherenotnans = np.where(np.nansum(np.isfinite(bad_pixels),axis=0)!=0)
         row_valid_pix = wherenotnans[0]
         col_valid_pix = wherenotnans[1]
         N_chunks = N_valid_pix//chunk_size
@@ -135,9 +142,9 @@ def findbadpix(cube, noisecube=None, badpixcube=None,chunks=20,mypool=None,med_s
             row_indices_list.append(_row_valid_pix)
             col_indices_list.append(_col_valid_pix)
 
-            data_list.append(cube[:,_row_valid_pix,_col_valid_pix])
-            noise_list.append(noisecube[:,_row_valid_pix,_col_valid_pix])
-            badpix_list.append(new_badpixcube[:,_row_valid_pix,_col_valid_pix])
+            data_list.append(data.data[:, _row_valid_pix, _col_valid_pix])
+            noise_list.append(noise[:, _row_valid_pix, _col_valid_pix])
+            badpix_list.append(bad_pixels[:, _row_valid_pix, _col_valid_pix])
 
         _row_valid_pix = row_valid_pix[((N_chunks-1)*chunk_size):N_valid_pix]
         _col_valid_pix = col_valid_pix[((N_chunks-1)*chunk_size):N_valid_pix]
@@ -145,24 +152,24 @@ def findbadpix(cube, noisecube=None, badpixcube=None,chunks=20,mypool=None,med_s
         row_indices_list.append(_row_valid_pix)
         col_indices_list.append(_col_valid_pix)
 
-        data_list.append(cube[:,_row_valid_pix,_col_valid_pix])
-        noise_list.append(noisecube[:,_row_valid_pix,_col_valid_pix])
-        badpix_list.append(new_badpixcube[:,_row_valid_pix,_col_valid_pix])
+        data_list.append(data.data[:, _row_valid_pix, _col_valid_pix])
+        noise_list.append(noise[:, _row_valid_pix, _col_valid_pix])
+        badpix_list.append(bad_pixels[:, _row_valid_pix, _col_valid_pix])
 
-        outputs_list = mypool.map(_task_findbadpix, zip(data_list,noise_list,badpix_list,
+        outputs_list = mypool.map(_task_findbadpix, zip(data_list, noise_list, badpix_list,
                                                                itertools.repeat(med_spec),
                                                                itertools.repeat(M_spline)))
-        for row_indices,col_indices,out in zip(row_indices_list,col_indices_list,outputs_list):
-            out_data,out_badpix,out_res = out
-            new_cube[:,row_indices,col_indices] = out_data
-            new_badpixcube[:,row_indices,col_indices] = out_badpix
-            res[:,row_indices,col_indices] = out_res
+        for row_indices, col_indices, out in zip(row_indices_list, col_indices_list, outputs_list):
+            out_data, out_badpix, out_res = out
+            data.data[:, row_indices, col_indices] = out_data
+            data.bad_pixels[:, row_indices, col_indices] = out_badpix
+            res[:, row_indices, col_indices] = out_res
 
-    return new_badpixcube, new_cube, res
+    return res
 
 
 
-def broaden(wvs,spectrum,R,mppool=None):
+def broaden(wvs, spectrum, R, mppool=None):
     """
     Broaden a spectrum to instrument resolution assuming a gaussian line spread function.
 
@@ -180,7 +187,7 @@ def broaden(wvs,spectrum,R,mppool=None):
     """
     if mppool is None:
         # Each wavelength processed sequentially
-        return _task_broaden((np.arange(np.size(spectrum)).astype(np.int),wvs,spectrum,R))
+        return _task_broaden((np.arange(np.size(spectrum)).astype(np.int), wvs, spectrum, R))
     else:
         conv_spectrum = np.zeros(spectrum.shape)
 
@@ -189,15 +196,15 @@ def broaden(wvs,spectrum,R,mppool=None):
         N_chunks = np.size(spectrum)//chunk_size
         indices_list = []
         for k in range(N_chunks-1):
-            indices_list.append(np.arange(k*chunk_size,(k+1)*chunk_size).astype(np.int))
-        indices_list.append(np.arange((N_chunks-1)*chunk_size,np.size(spectrum)).astype(np.int))
+            indices_list.append(np.arange(k*chunk_size, (k+1)*chunk_size).astype(np.int))
+        indices_list.append(np.arange((N_chunks-1)*chunk_size, np.size(spectrum)).astype(np.int))
         # Start parallelization
-        outputs_list = mppool.map(_task_broaden, zip(indices_list,
-                                                               itertools.repeat(wvs),
-                                                               itertools.repeat(spectrum),
+        outputs_list = mppool.map(_task_broaden,  zip(indices_list, 
+                                                               itertools.repeat(wvs), 
+                                                               itertools.repeat(spectrum), 
                                                                itertools.repeat(R)))
         # Retrieve results
-        for indices,out in zip(indices_list,outputs_list):
+        for indices, out in zip(indices_list, outputs_list):
             conv_spectrum[indices] = out
 
         return conv_spectrum
@@ -216,7 +223,7 @@ def _task_broaden(paras):
 
     conv_spectrum = np.zeros(np.size(indices))
     dwvs = wvs[1::] - wvs[0:(np.size(wvs) - 1)]
-    dwvs = np.append(dwvs,dwvs[-1])    # Size of each wavelength bin
+    dwvs = np.append(dwvs, dwvs[-1])    # Size of each wavelength bin
     for l, k in enumerate(indices):
         FWHM = wvs[k] / Rvec[k] # Full width at half maximum of the LSF at current wavelength
         sig = FWHM / (2 * np.sqrt(2 * np.log(2))) # standard deviation of the LSF (1D gaussian)
@@ -236,7 +243,7 @@ def _task_broaden(paras):
 def file_directory(file):
     return os.path.dirname(local(file))
 
-def LPFvsHPF(myvec,cutoff):
+def LPFvsHPF(myvec, cutoff):
     """
     Ask JB to write documentation!
     """
@@ -244,8 +251,10 @@ def LPFvsHPF(myvec,cutoff):
     #handling nans:
     wherenans = np.where(np.isnan(myvec_cp))
     window = int(round(np.size(myvec_cp)/(cutoff/2.)/2.))#cutoff
-    tmp = np.array(pd.DataFrame(np.concatenate([myvec_cp, myvec_cp[::-1]], axis=0)).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))
-    myvec_cp_lpf = np.array(pd.DataFrame(tmp).rolling(window=window, center=True).median().interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))[0:np.size(myvec), 0]
+    tmp = np.array(pd.DataFrame(np.concatenate([myvec_cp, myvec_cp[::-1]], axis=0)\
+        ).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))
+    myvec_cp_lpf = np.array(pd.DataFrame(tmp).rolling(window=window, center=True).median(\
+        ).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))[0:np.size(myvec), 0]
     myvec_cp[wherenans] = myvec_cp_lpf[wherenans]
 
 
@@ -272,8 +281,8 @@ def LPFvsHPF(myvec,cutoff):
 def get_spline_model(x_knots, x_samples, spline_degree=3):
     """
     Compute a spline based linear model.
-    If Y=[y1,y2,..] are the values of the function at the location of the node [x1,x2,...].
-    np.dot(M,Y) is the interpolated spline corresponding to the sampling of the x-axis (x_samples)
+    If Y=[y1, y2, ...] are the values of the function at the location of the node [x1, x2, ...].
+    np.dot(M, Y) is the interpolated spline corresponding to the sampling of the x-axis (x_samples)
 
 
     Args:
@@ -284,7 +293,7 @@ def get_spline_model(x_knots, x_samples, spline_degree=3):
             if np.size(x_knots) <= spline_degree, then spline_degree = np.size(x_knots)-1
 
     Returns:
-        M: Matrix of size (D,N) with D the size of x_samples and N the total number of nodes.
+        M: Matrix of size (D, N) with D the size of x_samples and N the total number of nodes.
     """
     if type(x_knots[0]) is list or type(x_knots[0]) is np.ndarray:
         x_knots_list = x_knots
@@ -292,14 +301,14 @@ def get_spline_model(x_knots, x_samples, spline_degree=3):
         x_knots_list = [x_knots]
 
     if np.size(x_knots_list) <= 1:
-        return np.ones((np.size(x_samples),1))
+        return np.ones((np.size(x_samples), 1))
     if np.size(x_knots_list) <= spline_degree:
         spline_degree = np.size(x_knots)-1
 
     M_list = []
     for nodes in x_knots_list:
         M = np.zeros((np.size(x_samples), np.size(nodes)))
-        min,max = np.min(nodes),np.max(nodes)
+        min, max = np.min(nodes), np.max(nodes)
         inbounds = np.where((min<x_samples)&(x_samples<max))
         _x = x_samples[inbounds]
 
