5b34f9e8db4f88d8dcf6c4af2c2d2a02ec4ea9aa
Shubh Agrawal <shubhagrawal30@gmail.com>
2021-07-21 17:45:56 -0400

    added bad pixel defaults to osiris

--- breads/instruments/OSIRIS.py
+++ breads/instruments/OSIRIS.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import axis
 from breads.instruments.instrument import Instrument
 import breads.utils as utils
 from warnings import warn
@@ -69,7 +70,13 @@ class OSIRIS(Instrument):
         
         self.valid_data_check()
 
-    def remove_bad_pixels(self, chunks=20, mypool=None, med_spec=None, nan_mask_boxsize=3):
+    def remove_bad_pixels(self, chunks=20, mypool=None, med_spec=None, nan_mask_boxsize=3, w=5):
+        if med_spec == "transmission" or med_spec == "pair subtraction":
+            img_mean = np.nanmean(self.data, axis=0)
+            x, y = np.unravel_index(np.nanargmax(img_mean), img_mean.shape)
+            med_spec = np.nanmedian(self.data[:,x-w:x+w, y-w:y+w], axis=(1,2))
+        elif med_spec == "default":
+            med_spec = None
         new_badpixcube, new_cube, res = \
             utils.findbadpix(self.data, self.noise, self.bad_pixels, chunks, mypool, med_spec, nan_mask_boxsize)
         self.bad_pixels = new_badpixcube
