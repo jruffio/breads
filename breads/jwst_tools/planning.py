35aa25b60765c0ca110a0a17f05fb29accb64b68
Marshall Perrin <mperrin@stsci.edu>
2025-10-11 01:49:09 -0400

    planning tool can support MRS too

--- breads/jwst_tools/planning.py
+++ breads/jwst_tools/planning.py
@@ -3,12 +3,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pysiaf
 
-
 def visualize_nrs_fov(comp_name, comp_sep, comp_pa, v3pa, center_on = 'star',
                           show_inner_diff_spikes=True, diff_spike_len = 2,
                           psf_core_check_radius=0.15,
-                          nirspec_aperture='ifu'):
-    """ Visualize NIRSpec IFU or slit FOV for a companion
+                          nirspec_aperture='ifu',
+                          verbose=False):
+    return visualize_jwst_ifu_fov(comp_name, comp_sep, comp_pa, v3pa, center_on,
+                          show_inner_diff_spikes, diff_spike_len,
+                          psf_core_check_radius,
+                          nirspec_aperture=nirspec_aperture,
+                          instrument='NIRSpec',
+                          verbose=verbose)
+
+def visualize_miri_mrs_fov(comp_name, comp_sep, comp_pa, v3pa, center_on = 'star',
+                          show_inner_diff_spikes=True, diff_spike_len = 2,
+                          psf_core_check_radius=0.15,
+                           miri_channel='2A',
+                          verbose=False):
+    return visualize_jwst_ifu_fov(comp_name, comp_sep, comp_pa, v3pa, center_on,
+                          show_inner_diff_spikes, diff_spike_len,
+                          psf_core_check_radius,
+                          instrument='MIRI', miri_channel=miri_channel,
+                          verbose=verbose)
+
+
+def visualize_jwst_ifu_fov(comp_name, comp_sep, comp_pa, v3pa, center_on = 'star',
+                          show_inner_diff_spikes=True, diff_spike_len = None,
+                          psf_core_check_radius=None,
+                          instrument='NIRSpec',
+                          nirspec_aperture='ifu',
+                          miri_channel='2A',
+                          verbose=False):
+    """ Visualize NIRSpec IFU or slit FOV, or MIRI MRS FOV, for a companion
 
     Parameters
     ----------
@@ -47,15 +73,34 @@ def visualize_nrs_fov(comp_name, comp_sep, comp_pa, v3pa, center_on = 'star',
 
     # Setup figure using siaf
     plt.figure()
-    nrs_siaf = pysiaf.Siaf('NIRSpec')
-    if nirspec_aperture == 'ifu':
-        for apname in nrs_siaf.apernames:
-            if 'IFU_SLICE' in apname:
-                nrs_siaf.apertures[apname].plot(frame='tel', color='gray', alpha=0.5)
-        ref_aperture = nrs_siaf.apertures['NRS_FULL_IFU']
+
+    if diff_spike_len is None:
+        diff_spike_len = 2 if instrument.lower()=='nirspec' else 4
+    if psf_core_check_radius is None:
+        psf_core_check_radius  =  0.15 if instrument.lower()=='nirspec' else 0.7  # TODO: make this better per channel?
+
+    slice_apernames = []
+    if instrument.lower()=='nirspec':
+        inst_siaf = pysiaf.Siaf('NIRSpec')
+        if nirspec_aperture == 'ifu':
+            for apname in inst_siaf.apernames:
+                if 'IFU_SLICE' in apname:
+                    inst_siaf.apertures[apname].plot(frame='tel', color='gray', alpha=0.5)
+                    slice_apernames.append(apname)
+            ref_aperture = inst_siaf.apertures['NRS_FULL_IFU']
+        else:
+            ref_aperture = inst_siaf.apertures['NRS_'+nirspec_aperture+'_SLIT']
+            ref_aperture.plot(frame='tel', color='gray', alpha=0.5)
+        aper_display_name = nirspec_aperture
     else:
-        ref_aperture = nrs_siaf.apertures['NRS_'+nirspec_aperture+'_SLIT']
-        ref_aperture.plot(frame='tel', color='gray', alpha=0.5)
+        inst_siaf = pysiaf.Siaf('MIRI')
+        for apname in inst_siaf.apernames:
+            # print(apname, f'MIRIFU_{miri_channel}' in apname)
+            if f'MIRIFU_{miri_channel}' in apname:
+                inst_siaf.apertures[apname].plot(frame='tel', color='gray', alpha=0.5)
+                slice_apernames.append(apname)
+        ref_aperture = inst_siaf.apertures[f'MIRIFU_CHANNEL{miri_channel}']
+        aper_display_name = f'MRS channel {miri_channel}'
 
     v2ref = ref_aperture.V2Ref
     v3ref = ref_aperture.V3Ref
@@ -73,6 +118,24 @@ def visualize_nrs_fov(comp_name, comp_sep, comp_pa, v3pa, center_on = 'star',
     else:
         raise RuntimeError('unknown value for center_on parameter')
     plt.plot(v2star, v3star, color='black', marker='o')
+    if verbose:
+        dv2 = v2star-v2ref
+        dv3 = v3star-v3ref
+        print(f"Offset star by {v2star-v2ref:.3f}, {v3star-v3ref:.3f} arcsec in (V2, V3) frame.")
+        print(f"Aperture {ref_aperture.AperName} has V3IdlYAngle =  {ref_aperture.V3IdlYAngle:.3f}")
+
+        if instrument.lower() == 'nirspec':
+            instrument_aper_pa = np.mod(v3pa + ref_aperture.V3IdlYAngle - 90, 360)  # extra 90 rot for NIRSpec IFU
+            print(f"For V3PA = {v3pa:.1f}, NIRSpec {ref_aperture.AperName} aperture PA (including pipeline IFUAlign rotation) is {instrument_aper_pa:.1f}")
+        else:
+            instrument_aper_pa= np.mod(v3pa + ref_aperture.V3IdlYAngle, 360)  # no extra 90 rot for MIRI
+            print(f"For V3PA = {v3pa:.1f}, MIRI {ref_aperture.AperName} aperture PA (including pipeline IFUAlign rotation) is {instrument_aper_pa:.1f}")
+
+        comp_aper_pa =  np.mod(instrument_aper_pa - comp_pa.to_value(u.deg), 360)
+        print(f"   Therefore companion PA ({comp_pa:.2f}) is oriented to local aperture angle {comp_aper_pa:.2f} from the star")
+        star_aper_pa = np.mod(comp_aper_pa + 180, 360)
+        print(f"   Therefore star is oriented to local aperture angle {star_aper_pa:.2f} from the companion")
+        print(f"    (for stpsf sim use offset {comp_sep_as * np.sin(np.deg2rad(star_aper_pa)):.3f}, {comp_sep_as * np.cos(np.deg2rad(star_aper_pa)):.3f} arcsec) ")
 
     for angle in range(6):
         ang_rad = np.deg2rad(angle * 60)
@@ -113,45 +176,45 @@ def visualize_nrs_fov(comp_name, comp_sep, comp_pa, v3pa, center_on = 'star',
              v3star + np.cos(comp_rel_pa_rad) * (comp_sep.to_value(u.arcsec) + 0.5),
              comp_name, color='blue')
 
-    if nirspec_aperture == 'ifu':
-        slice_V3IdlYAngle = nrs_siaf.apertures['NRS_IFU_SLICE00'].V3IdlYAngle
-        aplabel = 'IFUalign'
-        plt.text(298.7, -499.3, f"{aplabel} X axis. Bottom of NRS detectors", rotation=slice_V3IdlYAngle - 90, fontsize=8, color='green',
-                horizontalalignment='center', verticalalignment='center')
-        plt.text(301., -499.7, f"{aplabel} Y axis. ", rotation=slice_V3IdlYAngle - 180, fontsize=8, color='green',
-                horizontalalignment='center', verticalalignment='center')
-        plt.text(301.2, -496.9, "Top of NRS detectors", rotation=slice_V3IdlYAngle - 90, fontsize=8, color='green',
-                horizontalalignment='center', verticalalignment='center')
+    if instrument.lower() == 'nirspec':
+        if nirspec_aperture == 'ifu':
+            slice_V3IdlYAngle = inst_siaf.apertures['NRS_IFU_SLICE00'].V3IdlYAngle
+            aplabel = 'IFUalign'
+            plt.text(298.7, -499.3, f"{aplabel} X axis. Bottom of NRS detectors", rotation=slice_V3IdlYAngle - 90, fontsize=8, color='green',
+                    horizontalalignment='center', verticalalignment='center')
+            plt.text(301., -499.7, f"{aplabel} Y axis. ", rotation=slice_V3IdlYAngle - 180, fontsize=8, color='green',
+                    horizontalalignment='center', verticalalignment='center')
+            plt.text(301.2, -496.9, "Top of NRS detectors", rotation=slice_V3IdlYAngle - 90, fontsize=8, color='green',
+                    horizontalalignment='center', verticalalignment='center')
 
 
-        # Let's check if the star falls within any IFU slices, and if so warn the user
-        for apname in nrs_siaf.apernames:
-            if 'IFU_SLICE' in apname:
-                ap = nrs_siaf.apertures[apname]
-                x, y = ap.corners('tel', rederive=False)
-                x2, y2 = ap.closed_polygon_points('tel', rederive=False)
-                vertices = np.asarray([x,y]).transpose()
-                import matplotlib
-                polygon = matplotlib.patches.Polygon(vertices, closed=True, facecolor='pink', edgecolor='red', alpha=0.5)
-                # Note on polycon.contains_point:  from https://matplotlib.org/stable/api/_as_gen/matplotlib.patches.Patch.html
-                #  "The proper use of this method depends on the transform of the patch. [...]
-                #   The convention of checking against the transformed patch
-                #   stems from the fact that this method is predominantly used
-                #   to check if display coordinates (e.g. from mouse events) are
-                #   within the patch. If you want to do the above check with
-                #   data coordinates, you have to properly transform them first
-                transformed_star_coord = polygon.get_data_transform().transform((v2star, v3star))
+        else:
+            slice_V3IdlYAngle = ref_aperture.V3IdlYAngle
+            xtel, ytel = ref_aperture.idl_to_tel(0,0)
+            aplabel = nirspec_aperture
+            plt.text(xtel+0.5, ytel+0.5, f"{aplabel}", rotation=slice_V3IdlYAngle-90, fontsize=8, color='green',
+                    horizontalalignment='center', verticalalignment='center')
 
-                if polygon.contains_point(transformed_star_coord, radius=psf_core_check_radius):
-                    print(f"Caution: {apname} contains the PSF core, and may have saturated pixels and increased noise ")
-                    plt.gca().add_patch(polygon)
+    # Let's check if the star falls within any IFU slices, and if so warn the user
+    for apname in slice_apernames:
+        ap = inst_siaf.apertures[apname]
+        x, y = ap.corners('tel', rederive=False)
+        x2, y2 = ap.closed_polygon_points('tel', rederive=False)
+        vertices = np.asarray([x,y]).transpose()
+        import matplotlib
+        polygon = matplotlib.patches.Polygon(vertices, closed=True, facecolor='pink', edgecolor='red', alpha=0.5)
+        # Note on polycon.contains_point:  from https://matplotlib.org/stable/api/_as_gen/matplotlib.patches.Patch.html
+        #  "The proper use of this method depends on the transform of the patch. [...]
+        #   The convention of checking against the transformed patch
+        #   stems from the fact that this method is predominantly used
+        #   to check if display coordinates (e.g. from mouse events) are
+        #   within the patch. If you want to do the above check with
+        #   data coordinates, you have to properly transform them first
+        transformed_star_coord = polygon.get_data_transform().transform((v2star, v3star))
+
+        if polygon.contains_point(transformed_star_coord, radius=psf_core_check_radius):
+            print(f"Caution: {apname} contains the PSF core, and may have saturated pixels and increased noise ")
+            plt.gca().add_patch(polygon)
 
 
-    else:
-        slice_V3IdlYAngle = ref_aperture.V3IdlYAngle
-        xtel, ytel = ref_aperture.idl_to_tel(0,0)
-        aplabel = nirspec_aperture
-        plt.text(xtel+0.5, ytel+0.5, f"{aplabel}", rotation=slice_V3IdlYAngle-90, fontsize=8, color='green',
-                horizontalalignment='center', verticalalignment='center')
-
-    plt.title(f'{comp_name} at V3PA={v3pa} for NRS {nirspec_aperture.upper()}')
+    plt.title(f'{comp_name} at V3PA={v3pa} for {instrument} {aper_display_name.upper()}')
