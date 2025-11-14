708f4a50d3fe5606f959e66722f2a82135ee4830
Marshall Perrin <mperrin@stsci.edu>
2025-11-13 23:59:50 -0500

    add some more tests

--- breads/tests/test_jwst_tools.py
+++ breads/tests/test_jwst_tools.py
@@ -1,6 +1,11 @@
 import breads.jwst_tools
 
-def test_visualize_fov():
+def test_visualize_nrs_fov():
     import astropy.units as u
     breads.jwst_tools.visualize_nrs_fov('GJ 504 b', 2.5*u.arcsec, 315*u.degree, v3pa=272, center_on='comp')
+
+def test_visualize_mrs_fov():
+    import astropy.units as u
+    breads.jwst_tools.visualize_miri_mrs_fov('GJ 504 b', 2.5*u.arcsec, 315*u.degree, v3pa=272, center_on='comp')
+
 
