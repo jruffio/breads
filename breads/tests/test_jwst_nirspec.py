c9ca37714791dd3fa5ba4770c7443586c13166a5
jruffio <jruffio@ucsd.edu>
2026-04-24 10:01:43 -0700

    adding test data folder management.

--- breads/tests/test_jwst_nirspec.py
+++ breads/tests/test_jwst_nirspec.py
@@ -1,18 +1,22 @@
 import astropy.io.fits as fits
 import os
+import astropy
 
-os.environ['BREADS_DATA'] = '/stow/jruffio/data/breads_data'
-if not os.path.exists(os.environ['BREADS_DATA']):
-        os.mkdir(os.environ['BREADS_DATA'])
-jwst_test_data_path = os.path.join(os.environ['BREADS_DATA'],"jwst_test_data/")
+BREADS_DATA_ENV = os.getenv('BREADS_DATA')
+if BREADS_DATA_ENV is None:
+    jwst_test_data_path = os.path.join(str(astropy.utils.data._get_download_cache_loc()),'jwst_test_data')
+else:
+    jwst_test_data_path = os.path.join(os.environ['BREADS_DATA'], "jwst_test_data")
 if not os.path.exists(jwst_test_data_path):
         os.mkdir(jwst_test_data_path)
+print("The JWST test data will be downloaded in: {}".format(jwst_test_data_path))
+
+test_file = 'jw03399002001_03102_00001_nrs2_uncal.fits'
 
 def test_download_from_mast():
-    file = 'jw03399002001_03102_00001_nrs2_uncal.fits'
-    print('downloading {} -> {}'.format(file,jwst_test_data_path))
-    mast_file_url = f"https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:JWST/product/{file}"
+    print('downloading {} -> {}'.format(test_file,jwst_test_data_path))
+    mast_file_url = f"https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:JWST/product/{test_file}"
     hdul = fits.open(mast_file_url)
-    hdul.writeto(os.path.join(jwst_test_data_path,file),overwrite=True)
+    hdul.writeto(os.path.join(jwst_test_data_path,test_file),overwrite=True)
 
-    assert os.path.exists(os.path.join(jwst_test_data_path,file))
+    assert os.path.exists(os.path.join(jwst_test_data_path,test_file))
